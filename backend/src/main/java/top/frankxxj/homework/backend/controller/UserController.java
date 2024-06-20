e3bd0bd70230b957f9514de685f1b9714d6764a0
H.muller <1559525266@qq.com>
2024-06-20 21:41:35 +0800

    增加用户的接口

--- backend/src/main/java/top/frankxxj/homework/backend/controller/UserController.java
+++ backend/src/main/java/top/frankxxj/homework/backend/controller/UserController.java
@@ -3,52 +3,45 @@ package top.frankxxj.homework.backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import top.frankxxj.homework.backend.service.UserService;
 import top.frankxxj.homework.backend.user.User;
-import top.frankxxj.homework.backend.utils.R;
-
-import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    private final UserService userService;
+
     @Autowired
-    private UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
 
     @PostMapping("/register")
-    public R registerUser(@RequestBody User user) {
-        User user1 = userService.getUser(user.getEmail());
-        if (user1 != null) {
-            return R.error("注册用户已存在");
+    public ResponseEntity<String> registerUser(@RequestBody User user) {
+        User existingUser = userService.getUser(user.getEmail());
+        if (existingUser != null) {
+            return ResponseEntity.badRequest().body("注册用户已存在");
         }
-        // 对密码进行加密
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        String encodedPassword = passwordEncoder.encode(user.getPassword());
-        user.setPassword(encodedPassword);
 
         userService.registerUser(user);
-        return R.ok("成功注册");
+        return ResponseEntity.ok("成功注册");
     }
 
     @PostMapping("/login")
-    public R loginUser(@RequestParam String email,@RequestParam String password) {
-        User user1 = userService.getUser(email);
-        if(user1 == null){
-            return R.error("用户不存在");
+    public ResponseEntity<String> loginUser(@RequestParam String email, @RequestParam String password) {
+        User user = userService.getUser(email);
+        if (user == null) {
+            return ResponseEntity.badRequest().body("用户不存在");
         }
 
-        // 创建BCryptPasswordEncoder对象
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         // 验证密码
-        if (!passwordEncoder.matches(password, user1.getPassword())) {
-            return R.error("密码不正确");
+        if (!user.getPassword().equals(password)) {
+            return ResponseEntity.badRequest().body("密码不正确");
         }
 
-        return R.ok("登录成功");
-
+        return ResponseEntity.ok("登录成功");
     }
 }
 
--- backend/src/main/java/top/frankxxj/homework/backend/service/serviceImpl/UserServiceImpl.java
+++ backend/src/main/java/top/frankxxj/homework/backend/service/serviceImpl/UserServiceImpl.java
@@ -1,35 +1,35 @@
 package top.frankxxj.homework.backend.service.serviceImpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import top.frankxxj.homework.backend.service.UserService;
 import top.frankxxj.homework.backend.user.User;
 import top.frankxxj.homework.backend.user.UserRepository;
-import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
+    @Override
     public String registerUser(User user) {
         if (userRepository.findByEmail(user.getEmail()) != null) {
             return "Email is already in use!";
         }
 
-        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         userRepository.save(user);
         return "User registered successfully!";
     }
 
+    @Override
     public String loginUser(String email, String password) {
         User user = userRepository.findByEmail(email);
 
-        if (user == null || !bCryptPasswordEncoder.matches(password, user.getPassword())) {
+        if (user == null || !user.getPassword().equals(password)) {
             return "Invalid email or password!";
         }
 
