4aef4ff2a3a6184fb2df2f8e046390980104b8ad
Frank-Xiao2002 <Xiao670127@126.com>
2024-06-20 21:33:02 +0800

    modify bike controller

--- backend/src/main/java/top/frankxxj/homework/backend/bike/BikeController.java
+++ backend/src/main/java/top/frankxxj/homework/backend/bike/BikeController.java
@@ -1,8 +1,10 @@
 package top.frankxxj.homework.backend.bike;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/bike")
@@ -10,4 +12,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class BikeController {
     private final BikeService bikeService;
 
+    @PostMapping("/report/{bikeId}/broken")
+    public ResponseEntity<?> reportBroken(@PathVariable UUID bikeId) {
+        bikeService.reportBroken(bikeId);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/report/{bikeId}/fixed")
+    public ResponseEntity<?> reportFixed(@PathVariable UUID bikeId) {
+        bikeService.reportFixed(bikeId);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<?> findAll() {
+        return ResponseEntity.ok(bikeService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Bike> findById(@PathVariable UUID id) {
+        return ResponseEntity.ok(bikeService.findById(id));
+    }
 }
