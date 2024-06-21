fdaab47cbabc5d8ab89c35c12724ee1a4dd89e1b
Frank-Xiao2002 <Xiao670127@126.com>
2024-06-22 03:12:34 +0800

    add security and finish basic endpoints

--- backend/src/main/java/top/frankxxj/homework/backend/security/userrole/UserRoleRelationRepository.java
+++ backend/src/main/java/top/frankxxj/homework/backend/security/userrole/UserRoleRelationRepository.java
@@ -4,11 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import top.frankxxj.homework.backend.security.role.Role;
 import top.frankxxj.homework.backend.security.user.User;
 
-import java.util.List;
-
 public interface UserRoleRelationRepository extends JpaRepository<UserRoleRelation, Long> {
     void deleteByUserAndRole(User user, Role role);
-
-    List<UserRoleRelation> findByUser(User user);
-
 }
